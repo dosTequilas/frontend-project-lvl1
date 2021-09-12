4ec7213bd63ec1c950428a10ed5251549a7774f9
Evgeniy Larin <nothinghill15@gmail.com>
2021-09-12 20:01:03 +0300

    stage 3 rest files

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,8 @@
 #!/usr/bin/env node
+// import { name } from '../src/cli.js';
+import readlineSync from 'readline-sync';
+
 console.log('Welcome to the brain games!');
+const name = readlineSync.question('May I have your name?');
+
+console.log(`Hello, ${name}`);
